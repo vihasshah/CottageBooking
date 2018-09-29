4a5b042a5738618fa205c563d6873751bfb44b8b
Vihas Shah <vihasshah@gmail.com>
2018-09-29 11:18:08 +0530

    session check added

--- app/src/main/java/com/khushi/win10/cottagebooking/SplashActivity.java
+++ app/src/main/java/com/khushi/win10/cottagebooking/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.khushi.win10.cottagebooking.Helpers.Utils;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -14,10 +16,16 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                String loginCred = SplashActivity.this.getSharedPreferences(Utils.SHARED_PREF_NAME,MODE_PRIVATE).getString(Utils.LOGIN_PREF,null);
+                if(loginCred == null) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
 
-                startActivity(intent);
+                    startActivity(intent);
+                }else{
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
 
+                    startActivity(intent);
+                }
             }
         },1500);
 
