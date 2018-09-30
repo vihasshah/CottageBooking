4822700297681a4c50d74132f28bc0ccd477d96e
Vihas Shah <vihasshah@gmail.com>
2018-10-01 01:18:20 +0530

    collection created to hold data till app is running

--- app/src/main/java/com/khushi/win10/cottagebooking/ObjectHolder.java
+++ app/src/main/java/com/khushi/win10/cottagebooking/ObjectHolder.java
@@ -1,14 +1,17 @@
 package com.khushi.win10.cottagebooking;
 
+import com.khushi.win10.cottagebooking.Model.RentListModel;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Win10 on 13/09/2018.
  */
 
 public class ObjectHolder {
-    public  static ArrayList<RentListModel> rentListModel;
-    public static ArrayList<NewsModel> newsModel;
+    public final static List<RentListModel.DataBean> rentListModel = new ArrayList<>();
+    public final static ArrayList<NewsModel> newsModel = new ArrayList<>();
 
 
 }
